aa1c5473403c83d815d92fae17d0147d7c719cb3
minhtuan007 <damminhtuan2006@gmail.com>
2024-06-19 23:06:43 +0700

    up

--- js/hocsinh.js
+++ js/hocsinh.js
@@ -53,18 +53,6 @@ document.addEventListener('DOMContentLoaded', function() {
         new Hocsinh('43', 'Đàm Minh Tuấn', '12/03/2006', 'url("footage/hocsinh_footage/8.png")', 'slogan', '0987654321'),
         new Hocsinh('44', 'Nguyễn Như Tuệ', '05/02/2006', 'url("footage/hocsinh_footage/9.jpg")', 'slogan', '0987654321'),
         new Hocsinh('45', 'Nguyễn Thị Thúy Xuân', '01/08/2006', 'url("footage/hocsinh_footage/10.jpg")', 'slogan', '0987654321'),
-        
-        
-        // 'url("footage/hocsinh_footage/1.png")',
-        // 'url("footage/hocsinh_footage/2.png")',
-        // 'url("footage/hocsinh_footage/3.png")',
-        // 'url("footage/hocsinh_footage/4.jpg")',
-        // 'url("footage/hocsinh_footage/5.jpg")',
-        // 'url("footage/hocsinh_footage/6.jpeg")',
-        // 'url("footage/hocsinh_footage/7.jpg")',
-        // 'url("footage/hocsinh_footage/8.png")',
-        // 'url("footage/hocsinh_footage/9.jpg")',
-        // 'url("footage/hocsinh_footage/10.jpg")',
     ];
     console.log(avt[0].pic)
 
@@ -114,20 +102,12 @@ document.addEventListener('DOMContentLoaded', function() {
         mail.innerHTML = avt[avt_main].mail;
         phone.innerHTML = avt[avt_main].phone;
         slogan.innerHTML = avt[avt_main].slogan
-
-        // avt_right_idx = avt.indexOf(avtShow[4].style.backgroundImage);
-        // avt_right_idx = (avt_right_idx+1) % avt.length;
-        // avtShow[0].style.backgroundImage = avt[avt_right_idx];
-        // console.log(avtShow[2].style[1])
-        // console.log(avtShow[2].style.height)
-        // console.log(avtShow[2])
     });
 
     prev.addEventListener('click', function(){
         var slide = document.querySelector('.slide');
         var avtShow = document.querySelectorAll('.avt');
         slide.prepend(avtShow[avtShow.length-1]);
-        // avt_left_idx = avt.indexOf(avtShow[0].style.backgroundImage)
 
         if(avt_left_idx === 0){
             avt_left_idx = 45;
@@ -145,7 +125,6 @@ document.addEventListener('DOMContentLoaded', function() {
             avt_main = 43;
         } 
         
-
         avtShow[avtShow.length-1].style.backgroundImage = avt[avt_left_idx].pic;
         name.innerHTML = avt[avt_main].name;
         birth.innerHTML = avt[avt_main].birth;
